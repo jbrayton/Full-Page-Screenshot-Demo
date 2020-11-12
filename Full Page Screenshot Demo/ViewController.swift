f72305ee91a1adba3ca9b04b2ad5127dc47a2df1
John Brayton <jbrayton@goldenhillsoftware.com>
2020-11-12 09:30:06 -0500

    Initial Commit

---
+++ Full Page Screenshot Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Full Page Screenshot Demo
+//
+//  Created by John Brayton on 11/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
