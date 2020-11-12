40e8277c5db80fceafb7baa7379ccba60e3bb04b
John Brayton <jbrayton@goldenhillsoftware.com>
2020-11-12 10:06:50 -0500

    Should have been included in prior commit.

---
+++ Full Page Screenshot Demo/ScreenshotDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  ScreenshotDelegate.swift
+//  Full Page Screenshot Demo
+//
+//  Created by John Brayton on 11/12/20.
+//
+
+import Foundation
+
+class ScreenshotServiceDelegate : UIScreenshotServiceDelegate {
+    
+    
+    
+}
