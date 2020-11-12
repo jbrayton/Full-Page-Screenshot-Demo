21362b244d88397de2974d2f0cb83f15bb1604e2
John Brayton <jbrayton@goldenhillsoftware.com>
2020-11-12 09:36:32 -0500

    App that shows a WKWebView with a webpage.

--- Full Page Screenshot Demo/ViewController.swift
+++ Full Page Screenshot Demo/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    var webview: WKWebView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.systemBackground
+
+        self.webview = WKWebView()
+        self.webview.translatesAutoresizingMaskIntoConstraints = false
+        self.webview.clipsToBounds = true
+        self.view.addSubview(self.webview)
+        
+        self.view.addConstraint(self.webview.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor))
+        self.view.addConstraint(self.webview.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor))
+        self.view.addConstraint(self.webview.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor))
+        self.view.addConstraint(self.webview.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor))
+        
+        self.webview.load(URLRequest(url: URL(string: "https://www.meetup.com/cocoaheads-boston/")!))
+
+    }
 
 }
 
